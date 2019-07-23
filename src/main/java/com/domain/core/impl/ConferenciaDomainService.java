94913cc81006928923c88e5c61fcdd2647987a7e
Administrator <admin@example.com>
2019-07-23 17:20:40 -0500

    Inicio Restricciones

--- src/main/java/com/domain/core/impl/ConferenciaDomainService.java
+++ src/main/java/com/domain/core/impl/ConferenciaDomainService.java
@@ -1,7 +1,24 @@
 package com.domain.core.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.domain.Conferencia;
+import com.domain.Equipo;
 import com.domain.core.IConferenciaDomainService;
 
 public abstract class ConferenciaDomainService implements IConferenciaDomainService {
+
+  @Override
+  public void validamaximoconferencias() {
+    // TODO Auto-generated method stub
+    ArrayList<Equipo> equipos = new ArrayList<>(); 
+    
+    @SuppressWarnings("null")
+    int size = equipos.size();
+    
+    if (size > 3 ) {
+      System.out.println("Maximo de Equipos Alcanzado");
+    }
+  }
 
 }
