c40f8c97e023b4ba1e12dca1251863bc470778a7
Administrator <admin@example.com>
2019-07-23 17:27:40 -0500

    Update Restricciones

--- src/main/java/com/domain/core/impl/ConferenciaDomainService.java
+++ src/main/java/com/domain/core/impl/ConferenciaDomainService.java
@@ -1,9 +1,5 @@
 package com.domain.core.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import com.domain.Conferencia;
-import com.domain.Equipo;
 import com.domain.core.IConferenciaDomainService;
 
 public abstract class ConferenciaDomainService implements IConferenciaDomainService {
@@ -11,14 +7,6 @@ public abstract class ConferenciaDomainService implements IConferenciaDomainServ
   @Override
   public void validamaximoequipos() {
     // TODO Auto-generated method stub
-    ArrayList<Equipo> equipos = new ArrayList<>(); 
-    
-    @SuppressWarnings("null")
-    int size = equipos.size();
-    
-    if (size > 3 ) {
-      System.out.println("Maximo de Equipos Alcanzado");
-    }
   }
 
 }
